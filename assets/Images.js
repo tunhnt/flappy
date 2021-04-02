d6105a8c531ad3f25d155ffc999f6f4286770f73
tunhnt <43764640+tunhnt@users.noreply.github.com>
2021-04-02 22:00:48 +0200

    Add files via upload

---
+++ assets/Images.js
@@ -0,0 +1,8 @@
+export default Images = {
+  background: require("./img/background-day.png"),
+  floor: require("./img/floor.png"),
+  pipeTop: require("./img/pipe-core.png"),
+  pipeCore: require("./img/pipe-top.png"),
+  bird1: require("./img/bluebird-downflap.png"),
+  comarBird: require("./img/comar.png"),
+}
